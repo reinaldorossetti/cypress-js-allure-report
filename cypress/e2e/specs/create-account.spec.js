ec81790445f719fb60a6f7fb251f803891d3cbf2
vinicius-airess <vinicius.aires@byxcapital.com.br>
2025-02-04 16:19:10 -0300

    Negative login tests

--- cypress/e2e/specs/create-account.spec.js
+++ cypress/e2e/specs/create-account.spec.js
@@ -1,7 +1,7 @@
 import DataShipping from '../../fixtures/dataProducts.js'
 
-describe('Account', () => {
-	it('Create Account', () => {
+describe('User Authentication Tests', () => {
+	it('Should create an account successfully', () => {
 		const testData = DataShipping()
 
 		cy.writeFile('cypress/fixtures/user.json', {
@@ -14,12 +14,45 @@ describe('Account', () => {
 		cy.get('.message-success').should('be.visible')
 	})
 
-	it('Login', () => {
+	it('Should log in successfully', () => {
 		cy.fixture('user').then((user) => {
 			cy.visit('/')
 			cy.login(user.email, user.password)
-
 			cy.get(':nth-child(2) > .greet > .logged-in').should('be.visible')
 		})
 	})
 })
+
+describe('Negative Login Tests', () => {
+	it('Should fail with incorrect email and correct password', () => {
+		cy.fixture('user').then((user) => {
+			cy.visit('/')
+			cy.login(user.incorrectEmail, user.password)
+			cy.get(':nth-child(2) > .greet > .logged-in').should('be.visible')
+		})
+	});
+
+	it('Should fail with correct email and incorrect password', () => {
+		cy.fixture('user').then((user) => {
+			cy.visit('/')
+			cy.login(user.email, user.incorrectPassword)
+			cy.get(':nth-child(2) > .greet > .logged-in').should('be.visible')
+		})
+	});
+
+	it('Should fail with both incorrect email and password', () => {
+		cy.fixture('user').then((user) => {
+			cy.visit('/')
+			cy.login(user.incorrectEmail, user.incorrectPassword)
+			cy.get(':nth-child(2) > .greet > .logged-in').should('be.visible')
+		})
+	});
+
+	it('Should fail with empty email and password', () => {
+		cy.fixture('user').then((user) => {
+			cy.visit('/')
+			cy.login(user.emptyEmail, user.emptyPassword)
+			cy.get(':nth-child(2) > .greet > .logged-in').should('be.visible')
+		})
+	});
+});
